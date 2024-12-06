e2a1148009c879e13efd4b784902edd949f22869
Robert Tsui <rt.roberttsui@gmail.com>
2024-12-06 12:57:37 -0800

    new list creation view template

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key='secret1'
 
+@app.before_request
+def initialize_session():
+    if 'lists' not in session:
+        session['lists'] = []
+        
 @app.route("/")
 def index():
-    return "<h1>This is a todo starter!</h1>"
+    return redirect(url_for('get_lists'))
+
+@app.route("/lists", methods=['GET'])
+def get_lists():
+    return render_template("lists.html", lists=session['lists'])
+
+@app.route("/lists/new")
+def add_todo():
+    return render_template('new_list.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
