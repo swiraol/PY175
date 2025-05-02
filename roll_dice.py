5bea34f404cc539812e4b26184d2a70283d342d4
Robert Tsui <rt.roberttsui@gmail.com>
2025-05-01 18:47:51 -0700

    http version not displaying in the request line

--- roll_dice.py
+++ roll_dice.py
@@ -1,7 +1,7 @@
 import socket
 import random
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(('localhost', 3003))
 server_socket.listen()
 
@@ -16,16 +16,27 @@ while True:
     if (not request) or ('favicon.ico' in request):
         client_socket.close()
         continue
-    
+
     request_line = request.splitlines()[0]
+    print(f"Request Line: {request_line}")
 
     roll = random.randint(1, 6)
     response_body = f"{request_line}\n{roll}"
+    # http_method = request_line.split(' ')[0]
+    # print("http_method: ", http_method)
+    # http_version = request_line.split(' ')[2]
+    # print("http_version: ", http_version)
 
-    response = ("HTTP/1.1 200 OK\r\n"
-                f"Content-Length: {len(response_body)}\r\n"
-                "\r\n"
-                f"{response_body}\n")
+    response = (
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/plain\r\n"
+        f"Content-Length: {len(response_body)}\r\n"
+        "\r\n"
+        f"Request Line: {request_line}\r\n"
+        # f"{response_body}\n"
+        # f"HTTP Method: {http_method}\r\n"
+    )
+    print(f"Server response: {response}")
     
     client_socket.sendall(response.encode())
     client_socket.close()
