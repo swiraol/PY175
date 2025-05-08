bd4a7fd351313c55325669483470fdcd6258b76c
Robert Tsui <rt.roberttsui@gmail.com>
2025-05-08 15:33:48 -0700

    added template filter and registered it

--- book_viewer_starter/app.py
+++ book_viewer_starter/app.py
@@ -1,26 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import re
 
 app = Flask(__name__)
 print(f"The value of __name__ in this module is {__name__}")
 
+def in_paragraphs(text):
+    paragraphs = text.split('\n\n')
+    
+    paragraphs = [f"<p>{paragraph}</p>" for paragraph in paragraphs]
+    return "".join(paragraphs)
+
+app.jinja_env.filters['in_paragraphs'] = in_paragraphs
+
+@app.before_request
+def load_contents():
+    with open("book_viewer/data/toc.txt", "r") as file:
+        g.contents = file.readlines()
+
 @app.route("/")
 def index():
-    with open('book_viewer/data/toc.txt', 'r') as file:
-        chapter_titles = file.readlines()
-
-    return render_template('home.html', chapter_titles=chapter_titles)
+    return render_template('home.html', chapter_titles=g.contents)
 
 @app.route("/chapters/<page_num>")
 def chapter(page_num):
-    with open('book_viewer/data/toc.txt', 'r') as file:
-        chapter_titles = file.readlines()
-        print("chapter_titles: ", chapter_titles)
     with open(f"book_viewer/data/chp{page_num}.txt", 'r') as file:
-        chapter_content = file.readlines()
-    
+        chapter_content = file.read()
+ 
     return render_template('chapter.html', 
-                           chapter_content=chapter_content, 
-                           chapter_titles=chapter_titles,
+                           chapter_paragraphs=chapter_content, 
+                           chapter_titles=g.contents,
                            page_num=int(page_num))
 
 if __name__ == "__main__":
