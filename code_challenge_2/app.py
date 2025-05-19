d5bfef9ff11d7a08507858d5daa8c5063ac8f4c8
Robert Tsui <rt.roberttsui@gmail.com>
2025-05-19 09:10:48 -0700

    implemented code challenge with a g object

--- code_challenge_2/app.py
+++ code_challenge_2/app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, g
+import yaml
 
 app = Flask(__name__)
 
+@app.before_request
+def load_user_data():
+    with open('users.yaml') as file:
+        g.user_data = yaml.safe_load(file)
+
 @app.route("/")
 def index():
-    with open("templates/index.html", "r") as file:
-        return file.read()
+
+    return redirect('/users')
+
+@app.route("/users")
+def users():
+    
+    usernames = g.user_data.keys()
+
+    return render_template('users.html', usernames=usernames)
+
 if __name__ == '__main__':
     app.run(debug=True, port=5003)
