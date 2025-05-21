e850b5d4ae9de902de8114dcc4688adc563ab000
Robert Tsui <rt.roberttsui@gmail.com>
2025-05-20 18:33:42 -0700

    refactored app

--- code_challenge_2/app.py
+++ code_challenge_2/app.py
@@ -5,55 +5,52 @@ app = Flask(__name__)
 
 @app.template_filter()
 def show_other_users(usernames, username):
-    print("usernames: ", usernames)
-
+    """Returns a list of other users"""
     return [user for user in usernames if user != username]
 
 @app.template_filter()
-def get_user_info(usernames, user_info):
-    print("got user_info: ", user_info)
+def get_user_info(usernames):
+    """Returns a tuple of counts for total users and interests"""
     total_users = len(usernames)
-    print("total_users: ", total_users)
-    print("g.user_data: ", g.user_data)
+    total_interests = get_total_interests()
+
+    return (total_users, total_interests)
+
+def get_total_interests():
+    """A helper function to calculate total interests of all users"""
     user_data = g.user_data
     total_interests = 0
     for v in user_data.values():
         if not isinstance(v, dict):
             v = {}
         else:
-            print("v: ", v)
-            print("interests count: ", len(v.get('interests', [])))
             total_interests += len(v.get('interests', []))
-            print("total_interests: ", total_interests)
-
-    print("total_interests: ", total_interests)
-
-    return (total_users, total_interests)
+    
+    return total_interests
 
 @app.before_request
 def load_user_data():
+    """Loads the yaml file for the lifecycle of a request"""
     with open('users.yaml') as file:
         g.user_data = yaml.safe_load(file)
 
 @app.route("/")
 def index():
-
-    return redirect('/users')
+    """Redirects to the /users page"""
+    return redirect("url_for('users')")
 
 @app.route("/users")
 def users():
-    
+    """Shows all the users on the page"""
     usernames = g.user_data.keys()
 
     return render_template('users.html', usernames=usernames)
 
 @app.route("/user/<username>")
 def user(username):
-    print("requested username: ", username)
+    """Shows the user's email and interests data"""
     user_info = g.user_data.get(username, {}) # the dictionary of user data
-    print("user_info: ", user_info)
     usernames = g.user_data.keys() # the username keys
-    print("usernames: ", usernames)
     
     if not isinstance(user_info, dict):
         email = "N/A"
@@ -61,10 +58,8 @@ def user(username):
         g.user_data[username] = {}
     else:
         email = user_info.get('email', "N/A") # the email string
-        print("email: ", email)
         interests = ", ".join(user_info.get('interests', [])) # the interests list
         interests = interests if interests else "N/A"
-        print("interests: ", interests)
 
     return render_template('user.html', email=email, username=username, interests=interests, usernames=usernames, user_info=user_info)
 
