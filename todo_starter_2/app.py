ddef370f242c76e40cce537e4d1cbdda2edbecc1
Robert Tsui <rt.roberttsui@gmail.com>
2025-05-21 17:01:17 -0700

    added feature to display all todo lists

--- todo_starter_2/app.py
+++ todo_starter_2/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>This is a todo starter!</h1>"
+    lists = [
+        {'title': 'Lunch Groceries', 'todos': []},
+        {'title': 'Dinner Groceries', 'todos': []},
+    ]
+    return render_template('lists.html', lists=lists)
+
+@app.route('/new-list')
+def new_list():
+
+    return render_template('new_list.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
