fa06af0a3917f01017c721a9106f42780673444b
Robert Tsui <rt.roberttsui@gmail.com>
2025-06-09 19:46:32 -0700

    added ability to create new todo

--- todo_starter_2/app.py
+++ todo_starter_2/app.py
@@ -9,7 +9,7 @@ from flask import (Flask,
                    flash,
 )
 from werkzeug.exceptions import NotFound
-from todos.utils import error_for_list_title, find_list_by_id
+from todos.utils import error_for_list_title, find_list_by_id, error_for_todo_title
 
 app = Flask(__name__)
 
@@ -31,7 +31,6 @@ def add_todo_list():
 
 @app.route('/lists', methods=["GET"])
 def get_lists():
-
     return render_template('lists.html', lists=session['lists'])
 
 @app.route('/lists', methods=["POST"])
@@ -57,11 +56,32 @@ def create_list():
 
 @app.route('/lists/<list_id>', methods=["GET"])
 def show_list(list_id):
+    print("--- Inside show list ---")
+    print(f"Requesting ID: {list_id}")
+    print("Session['lists'] content at start of get_list:", session['lists'])
     all_lsts = session['lists']
     todo_lst = find_list_by_id(list_id, all_lsts)
     if todo_lst:
         return render_template('list.html', todo_lst=todo_lst)
     raise NotFound(description="List not found")
+
+@app.route('/lists/<list_id>/todos', methods=["POST"])
+def create_todo(list_id):
+    print("session['lists']: ", session['lists'])
+    todo_lst = find_list_by_id(list_id, session['lists'])
+    if not todo_lst:
+        raise NotFound(description="List not found")
+    
+    todo_title = request.form.get('todo', None)
+    if error_for_todo_title(todo_title, todo_lst['todos']):
+        todo_lst['todos'].append({'title': todo_title, 'id': str(uuid4()), 'completed': False})
+        success_message = error_for_todo_title(todo_title, todo_lst['todos'])
+        flash(success_message, "success")
+    else:
+        flash("Your title shouldn't be empty or should be up to 100 characters long.")
+    print(f"todo item: ", todo_lst['todos'])
+    
+    return render_template('list.html', todo_lst=todo_lst)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
--- todo_starter_2/todos/utils.py
+++ todo_starter_2/todos/utils.py
@@ -6,12 +6,20 @@ def error_for_list_title(title, lists):
         return "The title must be 100 characters or less."
     else:
         return None
+
+def error_for_todo_title(title, todo):
+    if 0 < len(title) <= 100:
+        return 'You created a new todo item'
+    else:
+        return None
     
 def find_list_by_id(list_id, all_lsts):
     for lst in all_lsts:
+        print(f"Comparing (repr): '{repr(list_id)}' vs '{repr(lst['id'])}'")
+        print(f"Lengths: '{len(list_id)}' vs '{len(lst['id'])}'")
+        print(f"Types: '{type(list_id)}' vs '{type(lst['id'])}'")
         if lst['id'] == list_id:
             return lst
-        else:   
-            return None
+    return None
 
     
