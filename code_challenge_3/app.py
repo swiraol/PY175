72a61b9bf0b2f4363f72929cf1c1b3c91ee35aed
Robert Tsui <rt.roberttsui@gmail.com>
2025-10-17 09:20:19 -0700

    more refactoring

--- code_challenge_3/app.py
+++ code_challenge_3/app.py
@@ -3,21 +3,18 @@ import yaml
 
 app = Flask(__name__)
 
-def get_total_interests(lst):
-    return len(lst)
+def get_total_interests(dict):
+    result_lst = []
+    for v in dict.values():
+        for interest in v['interests']:
+            result_lst.append(interest)
+    return len(result_lst)
 
 @app.before_request
 def load_contents():
     with open('users.yaml', 'r') as file:
         g.storage = yaml.safe_load(file)
-        g.users = [user for user in g.storage]
-
-        result_lst = []
-        for v in g.storage.values():
-            for interest in v['interests']:
-                result_lst.append(interest)
-        
-        g.interest_count = get_total_interests(result_lst)
+        g.interest_count = get_total_interests(g.storage)
 
 @app.route('/')
 def index():
@@ -29,11 +26,12 @@ def users():
 
 @app.route('/users/<name>')
 def user(name):
+    users = [user for user in g.storage]
     user_info = g.storage.get(name)
     if not user_info:
         return redirect(url_for('users'))
                 
-    return render_template('user.html', name=name, email=user_info['email'], interests=user_info['interests'])
+    return render_template('user.html', name=name, email=user_info['email'], interests=user_info['interests'], users=users)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5003)
